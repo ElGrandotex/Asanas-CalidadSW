81fd5eb7c495b80e7b04cf6f0391eb660e0da806
ElGrandotex <jose.verdezoto.moran@gmail.com>
2023-12-10 14:04:42 -0500

    Traduccion palabra compuesta
    Se modifico el metodo de traduccion para que encuentre en una palabra si existen varios morfemas

--- src/app/asanas/componets/traductor/traductor.component.ts
+++ src/app/asanas/componets/traductor/traductor.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { AsanasService } from '../../services/asanas.service';
+import { Morfema } from '../../interface/morfema';
 
 @Component({
   selector: 'com-traductor',
@@ -20,17 +21,58 @@ export class TraductorComponent {
     this.asanaSrv.obtenerMorfema();
   }
 
+  // traducir(){
+  //   this.traduccion = 'Traducción no encontrada';
+  //   this.palabra = this.palabraTraducir.nativeElement.value.toLowerCase();
+
+  //   for(const dato of this.asanaSrv.morfemas){
+  //     if(this.palabra === dato.morfema){
+  //       this.traduccion =  dato.traduccion
+  //     }
+  //   }
+  //   this.palabra = this.palabra.toUpperCase()
+  //   this.palabraTraducir.nativeElement.value = '';
+  //   this.controlResultado = true;
+  // }
+
+  public morfemasEncontrados : any[] =[];
   traducir(){
     this.traduccion = 'Traducción no encontrada';
     this.palabra = this.palabraTraducir.nativeElement.value.toLowerCase();
+    console.log(this.morfemasEncontrados);
+    this.morfemasEncontrados = [{
+      morfema: this.palabra,
+      traduccion: this.traduccion
+    }]
 
-    for(const dato of this.asanaSrv.morfemas){
-      if(this.palabra === dato.morfema){
-        this.traduccion =  dato.traduccion
+    // Recorre la palabra en busca de coincidencias con los morfemas
+    for (const dato of this.asanaSrv.morfemas) {
+      if (this.palabra.startsWith(dato.morfema)) {
+        this.morfemasEncontrados = [];
+        this.morfemasEncontrados.push({
+          morfema: dato.morfema,
+          traduccion: dato.traduccion,
+        });
+        this.palabra = this.palabra.substring(dato.morfema.length);
       }
     }
-    this.palabra = this.palabra.toUpperCase()
+    console.log(this.morfemasEncontrados);
+
+    // Si se encontraron morfemas, busca la traducción de la palabra completa
+    if (this.morfemasEncontrados.length > 0) {
+      for (const dato of this.asanaSrv.morfemas) {
+        if (this.palabra === dato.morfema) {
+          this.morfemasEncontrados.push({
+            morfema: dato.morfema,
+            traduccion: dato.traduccion,
+          });
+          break;
+        }
+      }
+    }
+
     this.palabraTraducir.nativeElement.value = '';
     this.controlResultado = true;
   }
+
 }
